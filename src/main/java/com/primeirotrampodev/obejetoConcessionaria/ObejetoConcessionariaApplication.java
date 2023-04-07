d2a94fbd93a7ec5c60407d52974852f6b498b995
Gabriel Miranda <gabrielmirandabarros98@gmail.com>
2023-04-06 23:08:07 -0300

    Objeto Concessionaria

---
+++ src/main/java/com/primeirotrampodev/obejetoConcessionaria/ObejetoConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.obejetoConcessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObejetoConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObejetoConcessionariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/obejetoConcessionaria/ObejetoConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.obejetoConcessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObejetoConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
